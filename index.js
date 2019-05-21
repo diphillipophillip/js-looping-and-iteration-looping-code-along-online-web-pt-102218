c1da6824262b75652ee5cc5c7849c7cbf1b536e2
Phillip DiPhillipo <diphillipophillip@gmail.com>
2019-05-21 15:28:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // Code your solutions in this file
 
-function writeCards() {
-  
+const names = ["Ada", "Bren"];
+const events = "birthday";
+
+function writeCards(names, events) {
+  for (let i = 0; i < names.length; i++) {
+    
+  }
 }
